a6c0bf605c5320fee7706c7901f448c6642815dd
Poya Ghahremani <poya.ghahremani@learnet.se>
2022-02-22 20:13:43 +0100

    update productinfo, css, m.m.

--- api.js
+++ api.js
@@ -1,64 +1,64 @@
-const imgAdd= document.getElementById("container")
+const imgAdd = document.getElementById("container");
 
-var info=" jahir made mistakelll"
-const makeRequest= async(url)=>{
-const response= await fetch(url)
+var info = " jahir made mistakelll";
+const makeRequest = async (url) => {
+  const response = await fetch(url);
 
-//console.log(response)
-//return response
-if(!response.ok){
-    const  message=`Error: ${response.status}`
-     throw new Error(message)
-}
-const data = await response.json()
-return data
+  //console.log(response)
+  //return response
+  if (!response.ok) {
+    const message = `Error: ${response.status}`;
+    throw new Error(message);
+  }
+  const data = await response.json();
+  return data;
+};
 
-}
+const getData = () => {
+  makeRequest("https://pokeapi.co/api/v2/pokemon?limit=15&offset=200")
+    .then((res) => {
+      console.log(res.results);
+      var values = res.results;
+      const getInfo = values.map((val) => {
+        makeRequest(val.url).then((res) => {
+          console.log(res);
+          console.log(res.forms[0].name);
+          //console.log(res.held_items[0].item[0].name)
 
-
-const  getData=()=>{
-    makeRequest("https://pokeapi.co/api/v2/pokemon?limit=15&offset=200")
-    .then((res)=>
-    {     console.log(res.results)
-        var values = res.results
-        const getInfo=values.map((val)=>{
-         
-             makeRequest(val.url).then((res)=>{
-                console.log(res)
-                console.log(res.forms[0].name)
-                //console.log(res.held_items[0].item[0].name)
-                
-
-                    return   imgAdd.innerHTML += ` 
+          return (imgAdd.innerHTML += ` 
                     <div class="product-card">
                         <div class="product-img img-one">  
                             <img src=${res.sprites.front_shiny} alt="" /> 
                          </div>
                         <div class="product-text">
-                            ${res.forms[0].name}
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt excepturi totam ducimus, nemo tenetur, quibusdam accusamus neque deserunt aliquid perferendis.</p>
+                        <div class="product-name">
+                        ${res.forms[0].name}<br>
+                        ID: ${res.id}
+                        </div>
+                            <div class="pokemonInfo">
+                             <hr>
+                            Weight: ${res.weight}<br>
+                            Order: ${res.order}<br>
+                            Height: ${res.height}<br>
+                            </div>
+
+                            
                         </div>
                         <div class="product-cart">
                             <button type="submit">Add to cart</button>
                         </div>
-                    </div> `; 
-
-             })
-           
+                    </div> `);
+        });
+      });
+      console.log(getInfo);
     })
-    console.log(getInfo)
-   
-    })
-    .catch((err)=>console.log(err))
- 
-    
-}
-    console.log(getData())
+    .catch((err) => console.log(err));
+};
+console.log(getData());
 
-    // <div>
-    //                      <p>${res.forms[0].name}<p/>
-    //                   
-    //                     <p>${res.held_items[1].item[0]}<p/>
-                       
-                    
-    //                 </div>
+// <div>
+//                      <p>${res.forms[0].name}<p/>
+//
+//                     <p>${res.held_items[1].item[0]}<p/>
+
+//                 </div>
